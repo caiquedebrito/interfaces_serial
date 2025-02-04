958a8a69d974f99a090ad66788e36d02fe1c136f
Caique <caiquedebritoo@gmail.com>
2025-02-04 19:04:39 -0300

    Feat: Add IRQ handler setup for BUTTON_A and BUTTON_B

--- interfaces_serial.c
+++ interfaces_serial.c
@@ -6,6 +6,8 @@
 #define BLUE_LED_PIN 11
 #define GREEN_LED_PIN 12
 
+static void irq_handler(uint gpio, uint32_t events);
+
 int main()
 {
     stdio_init_all();
@@ -28,8 +30,18 @@ int main()
     gpio_set_dir(BUTTON_B, GPIO_IN);
     gpio_pull_up(BUTTON_B);
 
+    // Configuração do IRQ para o botão A
+    gpio_set_irq_enabled_with_callback(BUTTON_A, GPIO_IRQ_EDGE_FALL, true, &irq_handler);
+
+    // Configuração do IRQ para o botão B
+    gpio_set_irq_enabled_with_callback(BUTTON_B, GPIO_IRQ_EDGE_FALL, true, &irq_handler);
+
     while (true) {
         printf("Hello, world!\n");
         sleep_ms(1000);
     }
+}
+
+static void irq_handler(uint gpio, uint32_t events) {
+
 }
