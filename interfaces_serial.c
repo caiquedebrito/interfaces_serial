0d489a64b6ffb19cacc0599863986a9d3e973da2
Caique <caiquedebritoo@gmail.com>
2025-02-08 09:14:12 -0300

    Refactor: Implement I2C and LED setup functions, and enhance main loop for character display and LED representation

--- interfaces_serial.c
+++ interfaces_serial.c
@@ -24,11 +24,6 @@
 
 ssd1306_t ssd;
 
-// #define UART_ID uart0
-// #define BAUD_RATE 115200
-// #define UART_TX_PIN 0
-// #define UART_RX_PIN 1
-
 bool blue_led_on = false;
 bool green_led_on = false;
 bool led_buffer[NUM_PIXELS]; // Buffer de LEDs
@@ -41,35 +36,115 @@ void copy_array(bool *dest, const bool *src); // Função para copiar um array p
 static inline void put_pixel(uint32_t pixel_grb); // Função para enviar um pixel para o buffer
 static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b); // Função para converter um pixel para um inteiro
 void set_one_led(uint8_t r, uint8_t g, uint8_t b); // Função para definir a cor de todos os LEDs
+void i2c_setup(); // Função para inicializar o I2C
+void led_setup(); // Função para inicializar os LEDs
+void irq_setup(); // Função para inicializar os IRQs
+void ws2812_setup(); // Função para inicializar o WS2812
+void print_number_on_leds(char n); // Função para imprimir um número na matriz de LEDs
+void print_char_on_display(char c); // Função para imprimir um caractere no display OLED
 
 int main()
 {
+    stdio_init_all();
+
+    led_setup();
+    i2c_setup();
+    irq_setup();
+    ws2812_setup();
+
+    while (true) {
+        if (stdio_usb_connected()) {
+            char c;
+            if (scanf("%c", &c) == 1) {
+                printf("Caractere lido: %c\n", c);
+
+                if (c == '.') {
+                    set_one_led(0, 0, 0);
+                    continue;
+                }
+
+                if (
+                    (c < '0') && (c > '9' && c < 'A')
+                    && (c > 'Z' && c < 'a')
+                    && (c > 'z')
+                ) continue;
+
+                print_char_on_display(c);
+                print_number_on_leds(c);
+            }
+        }
+        sleep_ms(10);
+    }
+}
+
+void ws2812_setup() {
     // Inicialização do PIO
     PIO pio = pio0;
     int sm = 0;
+
     uint offset = pio_add_program(pio, &ws2812_program);
 
-    stdio_init_all();
+    // Inicialização do programa do WS2812
+    ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, IS_RGBW);
 
-    // uart_init(UART_ID, BAUD_RATE);
+    set_one_led(0, 0, 0); // Desliga todos os LEDs da matriz
+}
 
-    //  // Configura os pinos GPIO para a UART
-    //  gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART); // Configura o pino 0 para TX
-    //  gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART); // Configura o pino 1 para RX
-
-    // Inicialização do I2C em 400 kHz
-    i2c_init(I2C_PORT, 400 * 1000);
-
-    // Configuração dos pinos SDA e SCL
-    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
-    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
-    gpio_pull_up(I2C_SDA);
-    gpio_pull_up(I2C_SCL);
-    ssd1306_init(&ssd, WIDTH, HEIGHT, false, I2C_ADDR, I2C_PORT);
-    ssd1306_config(&ssd);
+void print_char_on_display(char c) {
     ssd1306_fill(&ssd, false);
+    ssd1306_draw_char(&ssd, c, 64, 32);
     ssd1306_send_data(&ssd);
+}
 
+void print_number_on_leds(char n) {
+    printf("Imprimindo o número %c\n", n);
+    switch (n) {
+        case '0':
+            copy_array(led_buffer, zero);
+            break;
+        case '1':
+            copy_array(led_buffer, one);
+            break;
+        case '2':
+            copy_array(led_buffer, two);
+            break;
+        case '3':
+            copy_array(led_buffer, three);
+            break;
+        case '4':
+            copy_array(led_buffer, four);
+            break;
+        case '5':
+            copy_array(led_buffer, five);
+            break;
+        case '6':
+            copy_array(led_buffer, six);
+            break;
+        case '7':
+            copy_array(led_buffer, seven);
+            break;
+        case '8':
+            copy_array(led_buffer, eight);
+            break;
+        case '9':
+            copy_array(led_buffer, nine);
+            break;
+        default:
+            return;
+    }
+
+    set_one_led(100, 0, 0);
+}
+
+void irq_setup() {
+    // Configuração do IRQ para o botão A
+    gpio_set_irq_enabled_with_callback(BUTTON_A, GPIO_IRQ_EDGE_FALL, true, &irq_handler);
+
+    // Configuração do IRQ para o botão B
+    gpio_set_irq_enabled_with_callback(BUTTON_B, GPIO_IRQ_EDGE_FALL, true, &irq_handler);
+}
+    
+void led_setup() {
     // Configuração do pino de LED azul
     gpio_init(BLUE_LED_PIN);
     gpio_set_dir(BLUE_LED_PIN, GPIO_OUT);
@@ -87,83 +162,22 @@ int main()
     gpio_init(BUTTON_B);
     gpio_set_dir(BUTTON_B, GPIO_IN);
     gpio_pull_up(BUTTON_B);
+}
 
-    // Inicialização do programa do WS2812
-    ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, IS_RGBW);
+void i2c_setup() {
+    // Inicialização do I2C em 400 kHz
+    i2c_init(I2C_PORT, 400 * 1000);
 
-    // Configuração do IRQ para o botão A
-    gpio_set_irq_enabled_with_callback(BUTTON_A, GPIO_IRQ_EDGE_FALL, true, &irq_handler);
+    // Configuração dos pinos SDA e SCL
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SDA);
+    gpio_pull_up(I2C_SCL);
 
-    // Configuração do IRQ para o botão B
-    gpio_set_irq_enabled_with_callback(BUTTON_B, GPIO_IRQ_EDGE_FALL, true, &irq_handler);
-
-    set_one_led(0, 0, 0); // Desliga todos os LEDs da matriz
-
-    while (true) {
-        // if (uart_is_readable(UART_ID)) {
-        //     char c = uart_getc(UART_ID);
-        //     uart_putc(UART_ID, c);
-
-        //     uart_puts(UART_ID, " <- Eco do RP2\r\n");
-        // }
-        if (stdio_usb_connected()) {
-            char c;
-            if (scanf("%c", &c) == 1) {
-                printf("Caractere lido: %c\n", c);
-
-                if (c == '.') {
-                    set_one_led(0, 0, 0);
-                    continue;
-                }
-
-                if (
-                    (c < '0') && (c > '9' && c < 'A')
-                    && (c > 'Z' && c < 'a')
-                    && (c > 'z')
-                ) continue;
-
-                ssd1306_fill(&ssd, false);
-                ssd1306_draw_char(&ssd, c, 64, 32);
-                ssd1306_send_data(&ssd);
-
-                switch (c) {
-                    case '0':
-                        copy_array(led_buffer, zero);
-                        break;
-                    case '1':
-                        copy_array(led_buffer, one);
-                        break;
-                    case '2':
-                        copy_array(led_buffer, two);
-                        break;
-                    case '3':
-                        copy_array(led_buffer, three);
-                        break;
-                    case '4':
-                        copy_array(led_buffer, four);
-                        break;
-                    case '5':
-                        copy_array(led_buffer, five);
-                        break;
-                    case '6':
-                        copy_array(led_buffer, six);
-                        break;
-                    case '7':
-                        copy_array(led_buffer, seven);
-                        break;
-                    case '8':
-                        copy_array(led_buffer, eight);
-                        break;
-                    case '9':
-                        copy_array(led_buffer, nine);
-                        break;
-                }
-
-                set_one_led(100, 0, 0);
-            }
-        }
-        sleep_ms(10);
-    }
+    ssd1306_init(&ssd, WIDTH, HEIGHT, false, I2C_ADDR, I2C_PORT); // Inicializa o display OLED
+    ssd1306_config(&ssd); // Configura o display OLED
+    ssd1306_fill(&ssd, false); // Limpa o display OLED
+    ssd1306_send_data(&ssd); // Envia os dados para o display OLED
 }
 
 void turn_on_led(bool g, bool b) {
@@ -179,7 +193,7 @@ static void irq_handler(uint gpio, uint32_t events) {
 
     // Verifica se o botão pressionado foi o A
     if (gpio == BUTTON_A) {
-        if (current_time_a - last_time_a < DEBOUNCE_TIME) return;
+        if (current_time_a - last_time_a < DEBOUNCE_TIME) return; // Verifica se o tempo de debounce foi atingido
         last_time_a = current_time_a;
 
         printf("Botão A pressionado\n");
@@ -189,7 +203,7 @@ static void irq_handler(uint gpio, uint32_t events) {
 
     // Se chegou aqui, o botão pressionado foi o B
 
-    if (current_time_b - last_time_b < DEBOUNCE_TIME) return;
+    if (current_time_b - last_time_b < DEBOUNCE_TIME) return; // Verifica se o tempo de debounce foi atingido
     last_time_b = current_time_b;
 
     printf("Botão B pressionado\n");
